578b193f55b0b36d056aaf1c8853161ae0622dec
Lee <lcw9206@gmail.com>
2018-04-05 17:17:36 +0900

    로그인 기능 구현 및 오타 수정

--- myblog/accounts/urls.py
+++ myblog/accounts/urls.py
@@ -1,7 +1,10 @@
 # accounts/urls. py
 
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
+    url(r'^login/', auth_views.login, name='login',
+        kwargs={'template_name': 'accounts/login_form.html'}),
 ]
--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -6,5 +6,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('accounts/urls')),
+    url(r'^accounts/', include('accounts.urls')),
+
 ]
