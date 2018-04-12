3b19669f476d439665a0d99987418dcd7c676877
Lee <lcw9206@gmail.com>
2018-04-12 19:55:05 +0900

    media 관련 설정 세팅

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -1,11 +1,15 @@
 # myblog/urls.py
 
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
-
 ]
+
+
+urlpatterns += static('media/', document_root=settings.MEDIA_ROOT)
