4a1c737975d08cd79567ec9e608f14a7b70da76b
Lee <lcw9206@gmail.com>
2018-04-13 17:30:45 +0900

    post 모델 생성 및 admin 등록

--- myblog/post/admin.py
+++ myblog/post/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+# post/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content', 'post_thumbnail', 'created_at', 'updated_at']
--- myblog/post/models.py
+++ myblog/post/models.py
@@ -1,3 +1,36 @@
 # post/models.py
 
 from django.db import models
+from django.conf import settings
+from imagekit.models import ProcessedImageField
+from imagekit.processors import Thumbnail
+import datetime
+
+
+def set_post_path(instance, filename):
+    now = datetime.datetime.now()
+
+    path = 'images/post/{usename}/{year}_{month}_{day}/{micro}.{extension}'.format(
+        username=instance.user.username,
+        year=now.year,
+        month=now.month,
+        day=now.day,
+        micro=now.microsecond,
+        extension=filename.split('.')[-1]
+    )
+    return path
+
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    post_thumbnail = ProcessedImageField(
+        upload_to=set_post_path,
+        processors=[Thumbnail(200, 200)],
+        format='JPEG',
+        options={'quality' : 60},
+        blank=True
+    )
+    title = models.CharField(max_length=100, verbose_name='제목', help_text='100자 이내로 입력해주세요.')
+    content = models.CharField(max_length=150, verbose_name='내용', help_text='150자 이내로 입력해주세요.')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
