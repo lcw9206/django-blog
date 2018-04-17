4058c6401a948b90f6b8fce980d83dae1a9bcb70
Lee <lcw9206@gmail.com>
2018-04-17 20:39:41 +0900

    namespace 적용

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -4,12 +4,14 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 
 urlpatterns = [
+    url(r'^$', lambda r: redirect('post:post_list'), name='root'),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
-    url(r'^post/', include('post.urls')),
+    url(r'^post/', include('post.urls', namespace='post')),
 ]
 
 
