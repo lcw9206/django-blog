419d59016cb04b149a0f366b533b345ae4523ba1
Lee <lcw9206@gmail.com>
2018-04-30 15:35:07 +0900

    기능 추가: 댓글 목록보기

--- myblog/post/models.py
+++ myblog/post/models.py
@@ -32,10 +32,13 @@ class Category(models.Model):
 
 class Comment(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
-    post = models.ForeignKey('POST')
+    post = models.ForeignKey('POST', related_name='comments')
     content = models.CharField(max_length=60, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
 
 
 class Post(models.Model):
