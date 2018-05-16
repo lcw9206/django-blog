c2544ab9ca5def91a5d3a4bf0cddd2974a7dc67a
Lee <lcw9206@gmail.com>
2018-05-16 18:11:11 +0900

    기능 추가: 간단한 admin 페이지 커스터마이징

--- myblog/post/admin.py
+++ myblog/post/admin.py
@@ -6,14 +6,25 @@ from .models import Post, Category, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['user', 'id', 'title', 'content', 'post_thumbnail', 'category', 'created_at', 'updated_at']
+    list_display = ('user', 'id', 'title', 'content', 'post_thumbnail', 'category', 'created_at', 'updated_at', )
+    list_per_page = 5
+    list_editable = ('category', )
+    search_fields = ('user', )
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name']
+    list_display = ('name', 'category_post_count', )
+    ordering = ('name', )
+
+    def category_post_count(self, category_id):
+        return Post.objects.filter(category_id=category_id).count()
+
+    category_post_count.short_description = '카테고리 별 게시글 수'
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['author', 'post', 'content', 'created_at', 'updated_at']
+    list_display = ('author', 'post', 'content', 'created_at', 'updated_at', )
+    list_per_page = 10
+    search_fields = ('author', )
