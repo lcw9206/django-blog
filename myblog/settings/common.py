19164ff21b54eee997c9aaf7a9ac5092d17866d1
Lee <lcw9206@gmail.com>
2018-05-25 23:32:02 +0900

    기능 추가: AWS EC2 배포를 위한 설정

--- myblog/settings/common.py
+++ myblog/settings/common.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'vw%8)6(fw$^(ix)=#jodb7$*r2o(8_j==ig79vi_&bs7=2!=-1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- myblog/settings/prod.py
+++ myblog/settings/prod.py
@@ -1,10 +1,36 @@
 from .common import *
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.mysql'),
+        'HOST': os.environ['DB_HOST'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'NAME': os.environ['DB_NAME'],
+        'PORT': os.environ['DB_PORT'],
+    },
+}
+
+INSTALLED_APPS += ['storages']
+
+STATICFILES_STORAGE = 'myblog.storages.StaticS3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'myblog.storages.MediaS3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'ap-northeast-2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# 실 서비스 도메인 입력
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'localhost',
+    '.ap-northeast-2.compute.amazonaws.com',
+]
 
 STATIC_URL = '/static/'
 
